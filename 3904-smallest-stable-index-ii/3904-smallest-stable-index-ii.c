6a68585661336c662e7ab3822b2513af07088c1c
Humaira-kawal <iknowhumairakawal@gmail.com>
2026-09-05 20:22:18 +0530

    Time: 0 ms (100%), Space: 22.3 MB (91.67%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.c
@@ -0,0 +1,28 @@
+int firstStableIndex(int* nums, int numsSize, int k) {
+    int suffixMin[numsSize];
+
+    // Build suffix minimum array
+    suffixMin[numsSize - 1] = nums[numsSize - 1];
+
+    for (int i = numsSize - 2; i >= 0; i--) {
+        if (nums[i] < suffixMin[i + 1])
+            suffixMin[i] = nums[i];
+        else
+            suffixMin[i] = suffixMin[i + 1];
+    }
+
+    // Build prefix maximum while checking stability
+    int prefixMax = nums[0];
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > prefixMax)
+            prefixMax = nums[i];
+
+        int instability = prefixMax - suffixMin[i];
+
+        if (instability <= k)
+            return i;
+    }
+
+    return -1;
+}
